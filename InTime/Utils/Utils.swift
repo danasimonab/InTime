052aa939fc2592d6b39d8d1b1866706adc8d3390
Dana Buca <danasimonabuca@gmail.com>
2022-08-10 14:11:08 +0300

    Commit 10/08/2022

--- InTime/Utils/Utils.swift
+++ InTime/Utils/Utils.swift
@@ -13,6 +13,7 @@ class Utils : NSObject {
     
     
     
+    
     class func secondsToHoursMinutesSeconds (seconds : Int) -> (Int, Int, Int) {
       return (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
     }
