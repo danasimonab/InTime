9b2dd18fa81d4e6ba3c07990686844636c270713
Dana Buca <danasimonabuca@gmail.com>
2022-08-10 14:16:07 +0300

    test

--- InTime/Utils/Utils.swift
+++ InTime/Utils/Utils.swift
@@ -14,6 +14,9 @@ class Utils : NSObject {
     
     
     
+    
+    
+    
     class func secondsToHoursMinutesSeconds (seconds : Int) -> (Int, Int, Int) {
       return (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
     }
